caf87ba39e926d1c85f4f21adef9b769d20e96d0
m.kurahara <kuraharamasahiro@gmail.com>
2021-04-14 17:02:17 +0900

    一覧をコンポーネントに分割

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { UserListItemComponent } from './user-list-item/user-list-item.component';
+import { UserListComponent } from './user-list/user-list.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, HttpClientModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, UserListItemComponent, UserListComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
